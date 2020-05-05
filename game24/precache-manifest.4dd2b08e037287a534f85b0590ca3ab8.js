0eefc8991203907dc7e5b2a566881e11c8b94c3c
Yudong Wang <yudong.wang@outlook.com>
2020-05-05 11:59:01 +0200

    game24

---
+++ game24/precache-manifest.4dd2b08e037287a534f85b0590ca3ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3331e6842908ae4502af3489359f5f",
+    "url": "/game24/index.html"
+  },
+  {
+    "revision": "e8c51fa954047fda6c37",
+    "url": "/game24/static/css/main.c0234b3f.chunk.css"
+  },
+  {
+    "revision": "8f76442fd50bfa8d828e",
+    "url": "/game24/static/js/2.aae56486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game24/static/js/2.aae56486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c51fa954047fda6c37",
+    "url": "/game24/static/js/main.8357a6d1.chunk.js"
+  },
+  {
+    "revision": "be19d8f9dc6ba737dccf",
+    "url": "/game24/static/js/runtime-main.04bcbd90.js"
+  }
+]);
